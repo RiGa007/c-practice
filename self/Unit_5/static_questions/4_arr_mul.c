e1fece46eb0edcc8b6f417cab5dc5488375f37d6
Rishabh <gangwarrishab94@gmail.com>
2026-05-05 20:01:45 +0530

    multiplication of two arrays using malloc

---
+++ self/Unit_5/static_questions/4_arr_mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int N;
+
+    // Read the size of the arrays
+    scanf("%d", &N);
+
+    // Dynamically allocate memory for the three arrays
+    int *arr1 = (int *)malloc(N * sizeof(int));
+    int *arr2 = (int *)malloc(N * sizeof(int));
+    int *result = (int *)malloc(N * sizeof(int));
+
+
+    // Read elements into the first array
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr1[i]);
+    }
+
+    // Read elements into the second array
+    for (int i = 0; i < N; i++) {
+        scanf("%d", &arr2[i]);
+    }
+
+    // Multiply corresponding elements, store in result array, and print
+    for (int i = 0; i < N; i++) {
+        result[i] = arr1[i] * arr2[i];
+        printf("%d ", result[i]);
+    }
+    
+
+    return 0;
+}
